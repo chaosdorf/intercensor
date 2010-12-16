b1e5caee59243f06733262bdaeb8da4175932df2
Maximilian Gass <mxey@ghosthacking.net>
2010-12-16 21:18:42 +0100

    Implement verification for 02anondelivers

--- webif/lib/Intercensor/Challenge/02anondelivers.pm
+++ webif/lib/Intercensor/Challenge/02anondelivers.pm
@@ -2,6 +2,8 @@ package Intercensor::Challenge::02anondelivers;
 
 use Modern::Perl;
 use base qw(Intercensor::Challenge);
+use XML::LibXML;
+use LWP::Simple;
 
 sub id {
     '02anondelivers';
@@ -16,8 +18,8 @@ sub description {
     We have an encrypted token for you, which we need to deliver to another
     person via the 4chan imageboard.</p>
     <p>Your task is to plant it on <a
-    href="http://www.4chan.org/b/">4chan.org/b</a> and give us the URL to the
-    resulting thread</p>';
+    href="http://www.4chan.org/b/">4chan.org/b</a>. Create a new thread and put
+    {{{TOKEN}}} into the body. Give us the URL of the resulting thread</p>';
 }
 
 sub get_question {
@@ -27,7 +29,39 @@ sub get_question {
 
 sub verify_answer {
     my ($self, $user_id, $answer) = @_;
-    return ($answer eq $self->generate_token($user_id));
+
+    if ($answer !~ m{^http://boards.4chan.org/b/res}) {
+        return undef;
+    }
+
+    my $content = get($answer);
+    if (!defined $content) {
+        return undef;
+    }
+
+    my $tree;
+    {
+        # XML::LibXML likes to spam with warnings.
+        local $SIG{__WARN__} = sub { };
+        $tree = XML::LibXML->load_html(string => $content, recover => 1);
+    }
+
+    my $post = $tree->findnodes('//blockquote')->[0];
+
+    if (!defined $post) {
+        return undef;
+    }
+
+    my $text = $post->textContent;
+
+    if ($text =~ /\{{3}(.+?)\}{3}/) {
+        my $token = $1;
+        return ($token eq $self->generate_token($user_id));
+    }
+    else {
+        return undef;
+    }
+
 }
 
 1;

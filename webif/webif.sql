4c3a5821cd346625e197437d3fef62dafac35553
Maximilian Gass <mxey@ghosthacking.net>
2010-12-31 11:02:54 +0100

    Add SQL schema for PostgreSQL

--- webif/webif.sql
+++ webif/webif.sql
@@ -1,2 +1,20 @@
-CREATE TABLE users (id INTEGER PRIMARY KEY, username, password, salt);
-CREATE TABLE solved_challenges (challenge, user_id, solved_at);
+DROP TABLE IF EXISTS solved_challenges;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id serial,
+    name character varying(50),
+    password char(60),
+    PRIMARY KEY (id),
+    UNIQUE (name)
+);
+
+CREATE TABLE solved_challenges (
+    user_id integer,
+    challenge_id character varying(50),
+    solved_at timestamp with time zone,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+DROP INDEX IF EXISTS solved_challenges_user_id;
+CREATE INDEX solved_challenges_user_id ON solved_challenges (user_id);
